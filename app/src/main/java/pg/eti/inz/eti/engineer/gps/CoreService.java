c2075e51f0fcd6aff87be2e2901119f16e1cc9a3
ubap <ubap.dev@gmail.com>
2016-10-27 17:56:29 +0200

    fix merge

--- app/src/main/java/pg/eti/inz/eti/engineer/gps/CoreService.java
+++ app/src/main/java/pg/eti/inz/eti/engineer/gps/CoreService.java
@@ -1,38 +1,22 @@
 package pg.eti.inz.eti.engineer.gps;
 
-import android.app.NotificationManager;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-
-import java.util.LinkedList;
-import java.util.List;
 
 import pg.eti.inz.eti.engineer.R;
 import pg.eti.inz.eti.engineer.data.Trip;
 import pg.eti.inz.eti.engineer.utils.Log;
-
-/**
- * Created by ubap on 2016-10-15.
- */
 
 public class CoreService extends Service implements
         com.google.android.gms.location.LocationListener,
@@ -53,11 +37,10 @@ public class CoreService extends Service implements
 
     GoogleApiClient mGoogleApiClient;
     LocationRequest mLocationRequest;
-    LocationSettingsRequest.Builder builder;
 
     private final IBinder mGPSBinder = new GPSBinder();
 
-
+    // GoogleApiClient.ConnectionCallbacks
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         Log.d();
@@ -72,6 +55,7 @@ public class CoreService extends Service implements
         startLocationUpdates();
     }
 
+    // GoogleApiClient.ConnectionCallbacks
     @Override
     public void onConnectionSuspended(int i) {
 
@@ -102,21 +86,20 @@ public class CoreService extends Service implements
             tracking = false;
             return currTrip;
         }
-
-        public PendingResult<LocationSettingsResult> checkLocationSettings() {
-            return LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
-        }
     }
 
+    // Service
     @Override
     public void onCreate() {
     }
 
+    // Service
     @Override
     public void onDestroy() {
         Log.d();
     }
 
+    // Service
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d();
@@ -132,25 +115,27 @@ public class CoreService extends Service implements
         mGoogleApiClient.connect();
 
         // make this service as a foreground one
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext())
-                .setSmallIcon(R.drawable.apple_safari).setContentTitle("Trackowanie drogi").setContentText("Trwa");
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(getApplicationContext())
+                .setSmallIcon(R.drawable.apple_safari)
+                .setContentTitle("Trackowanie drogi")
+                .setContentText("Trwa");
 
-        this.startForeground(0, mBuilder.build());
+        this.startForeground(0, notificationBuilder.build());
 
         return START_STICKY;
     }
 
+    // Service
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        // other bind purposes
         return mGPSBinder;
     }
 
-    // LocationListener
+    //  com.google.android.gms.location.LocationListener
     @Override
     public void onLocationChanged(Location location) {
-
         Log.d(location.toString());
         if(tracking) {
             currTrip.addLocation(location);
